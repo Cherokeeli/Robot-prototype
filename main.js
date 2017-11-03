50822f6156b512fba55968ff8e32cab376e08e57
Lii Quan <quanlime@gmail.com>
2017-11-03 16:52:22 +0800

    animation event queue finished.

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ var shelf = new Shelf(temp);
 
 var robot1 = new Robot(1,2);
 var robot2 = new Robot(2,3);
-robot2.engageToWhere(4);
+robot2.engageToWhere(4,function() {
+    robot2.getItemFromShelf(shelf,1,1);
+    robot2.engageToWhere(1,function() {
+        robot2.getItemFromShelf(shelf,1,1);
+    });
+});
 robot1.engageToWhere(2,function() {
     robot1.getItemFromShelf(shelf,3,0);
 });
